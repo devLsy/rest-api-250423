c200819f6cc2852b5ecc01a27d1af3f8ff8f7222
이상엽 <infightersy@naver.com>
2025-05-14 11:44:59 +0900

    유효성 검증 테스트 중

--- src/main/java/com/test/lsy/restapi250423/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/test/lsy/restapi250423/common/handler/GlobalExceptionHandler.java
@@ -2,16 +2,26 @@ package com.test.lsy.restapi250423.common.handler;
 
 import com.test.lsy.restapi250423.common.model.ApiResponse;
 import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ApiResponse<Object>> handleValidationException(ValidationException ex) {
+        String errorMessage = ex.getMessage();
+
+        // 에러 메시지 로깅 (서버 측)
+        List<String> errors = Arrays.asList(errorMessage.split(";"));
+
         ApiResponse<Object> response = ApiResponse.fail("유효성 검증이 실패했습니다.", HttpStatus.BAD_REQUEST);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
--- src/main/java/com/test/lsy/restapi250423/common/utils/ValidationUtil.java
+++ src/main/java/com/test/lsy/restapi250423/common/utils/ValidationUtil.java
@@ -3,6 +3,7 @@ package com.test.lsy.restapi250423.common.utils;
 import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 
 import java.util.List;
@@ -13,17 +14,21 @@ public class ValidationUtil {
     public static void invokeErrors(String className, BindingResult br) throws ValidationException {
 
         if(br.hasErrors()) {
-
             log.error("Validation Error @Class={}", className);
 
             List<ObjectError> errList = br.getAllErrors();
-
             StringBuilder errorMsg = new StringBuilder();
 
-            for(ObjectError err : errList) {
-                String msg = err.getDefaultMessage();
-                log.error(" - {}", msg);
-                errorMsg.append(msg).append("; ");
+            for (ObjectError err : br.getAllErrors()) {
+                if (err instanceof FieldError) {
+
+                    FieldError fieldError = (FieldError) err;
+                    String fieldName = fieldError.getField();
+                    String errorMessage = fieldError.getDefaultMessage();
+
+                    log.error("Detailed Error: {}", fieldError.toString());     // 상세 오류 출력
+                    errorMsg.append(fieldName).append(": ").append(errorMessage).append("; ");
+                }
             }
 
             throw new ValidationException(errorMsg.toString());
